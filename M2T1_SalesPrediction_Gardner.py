d53ed9ce5ef3856aef29359d1c4c7db21b8181a3
gardnerm4128 <31700573+gardnerm4128@users.noreply.github.com>
2017-09-06 11:06:56 -0400

    Add files via upload

---
+++ M2T1_SalesPrediction_Gardner.py
@@ -0,0 +1,15 @@
+# cti 110
+# M2T1 - Sales Prediction
+# 8/30
+# Gardner
+
+# Suggested variables: totalSales, annualProfit
+
+# ask the user for total sales
+totalSales = int(input("Type the total sales: "))
+
+#calculate the profit amount
+annualProfit = 0.23 * totalSales
+
+#print the profit amount
+print(annualProfit)
